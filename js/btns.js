958a2b68ec462777137b40662c2a5f6a1c587243
Erik E <singular1958@outlook.com>
2023-01-23 20:48:23 -0600

    encriptador reto alura

---
+++ js/btns.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector("#botonEncriptado");
+btn.onclick = encriptar;
+
+var btn2 = document.querySelector("#botonDencriptado");
+btn2.onclick = desencriptar;
+
+var btn3 = document.querySelector("#botonCopiar");
+btn3.onclick = copiar;
+
